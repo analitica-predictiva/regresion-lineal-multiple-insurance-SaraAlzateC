3199ce765e36757911136d9fa2ef4748f5f7765a
SaraAlzateC <116410244+SaraAlzateC@users.noreply.github.com>
2022-11-29 21:54:05 -0500

    Intento 12

--- preguntas.py
+++ preguntas.py
@@ -74,8 +74,10 @@ def pregunta_03():
     # Importe OneHotEncoder
    
    
-    from sklearn.compose import make_column_selector, make_column_transformer
-    from sklearn.feature_selection import SelectKBest, f_regression
+    from sklearn.compose import make_column_selector 
+    from sklearn.compose import make_column_transformer
+    from sklearn.feature_selection import SelectKBest 
+    from sklearn.feature_selection import f_regression
     from sklearn.linear_model import LinearRegression
     from sklearn.model_selection import GridSearchCV
     from sklearn.pipeline import Pipeline
